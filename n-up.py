b1b250b25729546b03cd9df0174a8a6b12a35eb6
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-24 22:45:05 +0000

    Update n-up.py

--- n-up.py
+++ n-up.py
@@ -1,5 +1,4 @@
 import sys
-#import math
 import PyPDF2
 
 if (len(sys.argv) != 4):
@@ -9,16 +8,20 @@ if (len(sys.argv) != 4):
 	
 inputPDF = PyPDF2.PdfFileReader(open(sys.argv[2], "rb"))
 
+scaleFactor = 0.25
 outputPDF = PyPDF2.PdfFileWriter()
 pageWidth = inputPDF.getPage(0).mediaBox[2] - inputPDF.getPage(0).mediaBox[0]
+scaledWidth = pageWidth * scaleFactor
 pageHeight = inputPDF.getPage(0).mediaBox[3] - inputPDF.getPage(0).mediaBox[1]
+scaledHeight = pageHeight * scaleFactor
+
 outputPage = outputPDF.addBlankPage(pageWidth, pageHeight)
 
 for pageNumber in range (0, inputPDF.getNumPages()):
 	print(str(pageNumber))
 inputPage = inputPDF.getPage(0)
-inputPage.scaleBy(0.25)
-outputPage.mergeRotatedTranslatedPage(inputPage, 90, 0, 0, False)
+inputPage.scaleBy(scaleFactor)
+outputPage.mergeRotatedTranslatedPage(inputPage, 90, scaledWidth, scaledHeight, False)
 
 outputHandle = open(sys.argv[3], "wb")
 outputPDF.write(outputHandle)
