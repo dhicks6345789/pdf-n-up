9f1d5014e4d581692d45f18cf99c4393dc34c629
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-24 21:54:11 +0000

    Update n-up.py

--- n-up.py
+++ n-up.py
@@ -11,9 +11,9 @@ inputPDF = PyPDF2.PdfFileReader(open(sys.argv[2], "rb"))
 outputPDF = PyPDF2.PdfFileWriter()
 for pageNumber in range (0, inputPDF.getNumPages()):
 	print("A" + str(pageNumber) + "B")
-#lhs = input1.getPage(iter)
-#rhs = input1.getPage(iter+1)
-#lhs.mergeTranslatedPage(rhs, lhs.mediaBox.getUpperRight_x(),0, True)
+outputPage = outputPDF.getPage(1)
+inputPage = inputPDF.getPage(1)
+outputPage.mergeTranslatedPage(inputPage, outputPage.mediaBox.getUpperRight_x(),0, True)
 #output.addPage(lhs)
 #print (str(iter) + " "),
 #sys.stdout.flush()
