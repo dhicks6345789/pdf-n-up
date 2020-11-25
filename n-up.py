5dd6b6f6e4732056da26249e60a7e5f25a072084
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-25 12:45:20 +0000

    Update n-up.py

--- n-up.py
+++ n-up.py
@@ -7,7 +7,6 @@ if (len(sys.argv) != 4):
 	sys.exit(1)
 	
 inputPDF = PyPDF2.PdfFileReader(open(sys.argv[2], "rb"))
-
 outputPDF = PyPDF2.PdfFileWriter()
 pageWidth = inputPDF.getPage(0).mediaBox[2] - inputPDF.getPage(0).mediaBox[0]
 scaledWidth = pageWidth / 2
@@ -21,25 +20,15 @@ outputPDF.addBlankPage(pageWidth, pageHeight)
 pageTransforms = [[300,0,270],[0,0,270],[0,0,270],[0,0,270],[0,0,90],[0,0,90],[0,0,90],[0,0,90]]
 for pageNumber in range (0, inputPDF.getNumPages()):
 	outputPDF.addPage(inputPDF.getPage(pageNumber).rotateClockwise(pageTransforms[pageNumber][2]))
+	
+outputHandle = open(sys.argv[3], "wb")
+outputPDF.write(outputHandle)
 
-#outputPage.mergeTranslatedPage(inputPDF.getPage(0).rotateCounterClockwise(90), 300, 0, False)
-
-#inputPage = inputPDF.getPage(0).rotateClockwise(90)
-#inputPage.scaleBy(0.25)
-##inputPage = inputPage.rotateCounterClockwise(270)
-#outputPage.mergeTranslatedPage(inputPage, 300, 0, False)
-#outputPDF.addPage(outputPage)
-
-
-
-#output.addPage(input1.getPage(i).rotateCounterClockwise(90))
-
-
-#for pageNumber in range (0, inputPDF.getNumPages()):
-#	inputPage = inputPDF.getPage(pageNumber)
-#	#inputPage.scaleBy(0.25)
-#	inputPage.rotateCounterClockwise(270)
-#	outputPage.mergeTranslatedPage(inputPage, pageTransforms[pageNumber][0], pageTransforms[pageNumber][1], False)
+inputPDF = PyPDF2.PdfFileReader(open(sys.argv[3], "rb"))
+outputPDF = PyPDF2.PdfFileWriter()
+outputPDF.addBlankPage(pageWidth, pageHeight)
+for pageNumber in range (0, inputPDF.getNumPages()):
+	outputPDF.mergeTranslatedPage(inputPDF.getPage(pageNumber), pageTransforms[pageNumber][0], pageTransforms[pageNumber][1], False)
 
 outputHandle = open(sys.argv[3], "wb")
 outputPDF.write(outputHandle)
