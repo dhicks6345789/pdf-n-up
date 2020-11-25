9cf60b0735a1d8f077d3adabf325bf302ab26642
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2020-11-25 13:38:14 +0000

    Update n-up.py

--- n-up.py
+++ n-up.py
@@ -15,7 +15,7 @@ scaledHeight = pageHeight / 4
 outputPDF = PyPDF2.PdfFileWriter()
 outputPage = outputPDF.addBlankPage(pageWidth, pageHeight)
 
-pageTransforms = [[scaledWidth,0,270],[0,0,270],[0,0,270],[0,0,270],[0,0,90],[0,0,90],[0,0,90],[0,0,90]]
+pageTransforms = [[scaledWidth,scaledHeight*4,270],[0,0,270],[0,0,270],[0,0,270],[0,0,90],[0,0,90],[0,0,90],[0,0,90]]
 for pageNumber in range (0, inputPDF.getNumPages()):
 	if pageNumber == 0:
 		outputPage.mergeRotatedScaledTranslatedPage(inputPDF.getPage(pageNumber), pageTransforms[pageNumber][2], 0.3536, pageTransforms[pageNumber][0], pageTransforms[pageNumber][1],True)
